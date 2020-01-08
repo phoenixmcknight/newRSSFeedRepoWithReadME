464d4a1a7365984fc563e91e08207a9fad29adc3
phoenixmcknight <phoenixmcknight@gmail.com>
2020-01-07 23:44:54 -0500

    moves Feed App to new Repo

---
+++ RSS-Reader-AppTests/RSS_Reader_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  RSS_Reader_AppTests.swift
+//  RSS-Reader-AppTests
+//
+//  Created by Phoenix McKnight on 12/17/19.
+//  Copyright © 2019 Phoenix McKnight. All rights reserved.
+//
+
+import XCTest
+@testable import RSS_Reader_App
+
+class RSS_Reader_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
